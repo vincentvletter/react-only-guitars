4a0c3953fc53e993810ede35c9d48eba36a76587
vincentvletter <vletter@msn.com>
2022-04-28 14:03:26 +0200

    changed-review

--- src/pages/Review.js
+++ src/pages/Review.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useForm} from "react-hook-form";
 import {useHistory, useParams} from "react-router-dom";
 import axios from "axios";
